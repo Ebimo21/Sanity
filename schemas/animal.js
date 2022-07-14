fcccf27755e5a72442c4ac41b6646d759cac7b24
ebimo21 <ebi4jah15@gmail.com>
2022-07-14 20:49:07 +0100

    first commit

---
+++ schemas/animal.js
@@ -0,0 +1,18 @@
+export default {
+    name: 'animal',
+    type: 'document',
+      title: 'Animal',
+    fields: [
+      {
+        name: 'name',
+        type: 'string',
+        title: 'Name'
+      },
+
+      {
+        name: 'type',
+        type: 'string',
+        title: 'Type'
+      }
+    ]
+  }
